f53f5dd20d4eaaa10baf7a4d4b702caf873b25c6
zjac197 <zjac197@live.rhul.ac.uk>
2023-01-29 15:54:37 +0000

    begun emergency braking system program

---
+++ program/catkin_ws/src/driving_programs/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/kacper/fyp/PROJECT/program/catkin_ws/src/driving_programs/src
+# Build directory: /home/kacper/fyp/PROJECT/program/catkin_ws/src/driving_programs/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ program/catkin_ws/src/driving_programs/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/kacper/fyp/PROJECT/program/catkin_ws/src/driving_programs/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/opt/ros/melodic'.split(';') if '/opt/ros/melodic' != '' else []

815e50ce3a92fade89415c42ac6ba07832805185
zjac197 <zjac197@live.rhul.ac.uk>
2023-03-06 16:54:44 +0000

    converted emergency brake program to a class

--- program/catkin_ws/src/driving_programs/src/emergency_brake.py
+++ program/catkin_ws/src/driving_programs/src/emergency_brake.py
@@ -3,24 +3,29 @@ import rospy
 from sensor_msgs.msg import LaserScan
 from ackermann_msgs.msg import AckermannDriveStamped
 
-def scan_listener(scan_data):
-    # print("distance to object: ", scan_data.ranges[360])
-    if scan_data.ranges[540] < 0.8: #condition on how close wall is
-        ack_data.drive.speed = 0 #stops car if true
-        # print("Stop car")
-    else:
-        ack_data.drive.speed = 2 #keeps speed of car at 2m/s
-    pub_drive.publish(ack_data) #publishes results
+class Emergency_brake:
+
+    def __init__(self):
+        #initialise Subscribers and Publishers
+        sub_scanner = rospy.Subscriber("scan", LaserScan, self.scan_listener)
+        self.pub_drive = rospy.Publisher("drive", AckermannDriveStamped, queue_size = 1)
+        self.ack_data = AckermannDriveStamped()
+        self.rate = rospy.Rate(500)
+
+    def scan_listener(self, scan_data):
+        front_scan = scan_data.ranges[540] 
+        if front_scan < 0.8: #condition on how close wall is
+            self.ack_data.drive.speed = 0 #stops car if true
+            print("Stop car")
+        else:
+            self.ack_data.drive.speed = 2 #keeps speed of car at 2m/s
+        self.pub_drive.publish(self.ack_data) #publishes results
 
 if __name__ == '__main__':
     try:
         print("program begin")
         rospy.init_node("brake_car")
-        #initialise Subscribers and Publishers
-        sub_scanner = rospy.Subscriber("scan", LaserScan, scan_listener)
-        pub_drive = rospy.Publisher("drive", AckermannDriveStamped, queue_size = 1)
-        ack_data = AckermannDriveStamped()
-        rate = rospy.Rate(500)
+        Emergency_brake()
         rospy.spin()
     except rospy.ROSInterruptException:
         pass

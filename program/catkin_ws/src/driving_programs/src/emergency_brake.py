93d147d055dec54e0672463a4ce7a1f08a2a5559
zjac197 <zjac197@live.rhul.ac.uk>
2023-03-11 15:28:19 +0000

    created unit tests that run to check emergency brake works

--- program/catkin_ws/src/driving_programs/src/emergency_brake.py
+++ program/catkin_ws/src/driving_programs/src/emergency_brake.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 import rospy
+import unittest
+import rostest
 from sensor_msgs.msg import LaserScan
 from ackermann_msgs.msg import AckermannDriveStamped
+
 
 class Emergency_brake:
 
@@ -10,22 +13,23 @@ class Emergency_brake:
         sub_scanner = rospy.Subscriber("scan", LaserScan, self.scan_listener)
         self.pub_drive = rospy.Publisher("drive", AckermannDriveStamped, queue_size = 1)
         self.ack_data = AckermannDriveStamped()
-        self.rate = rospy.Rate(500)
+        self.threshold = 0.5 #max distance from wall allowed
 
     def scan_listener(self, scan_data):
-        front_scan = scan_data.ranges[540] 
-        if front_scan < 0.8: #condition on how close wall is
-            self.ack_data.drive.speed = 0 #stops car if true
+        front_scan = min(scan_data.ranges)
+        if front_scan < self.threshold: #condition on how close wall is
+            self.ack_data.drive.speed = 0.0 #stops car if true
             print("Stop car")
         else:
-            self.ack_data.drive.speed = 2 #keeps speed of car at 2m/s
+            self.ack_data.drive.speed = 2.0 #keeps speed of car at 2m/s
         self.pub_drive.publish(self.ack_data) #publishes results
+
 
 if __name__ == '__main__':
     try:
         print("program begin")
         rospy.init_node("brake_car")
-        Emergency_brake()
+        brake = Emergency_brake()
         rospy.spin()
     except rospy.ROSInterruptException:
         pass
